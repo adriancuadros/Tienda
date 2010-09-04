1837e76e5d579d8b8ae4a4da69dc1093a4a8529d
Adrian Cuadros <adrian@innku.com>
2010-09-04 13:36:34 -0500

    Vistas

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ActiveRecord::Base
   validates_presence_of     :price
   
   validates_numericality_of :price
-  validates_numericality_of :units, :allow_blank => true
+  validates_numericality_of :units, :allow_blank => true  
 end
