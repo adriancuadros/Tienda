a995ac528f1c32a75c5fd525e1ff89ba11aa77b3
Adrian Cuadros <adrian@innku.com>
2010-09-04 13:26:14 -0500

    Numericality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
-  validates_presence_of   :name
-  validates_presence_of   :price
+  validates_presence_of     :name
+  validates_presence_of     :price
+  
+  validates_numericality_of :price
+  validates_numericality_of :units, :allow_blank => true
 end
