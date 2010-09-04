48ffe91b8aa3d240feefeb82579c085628a6da44
Adrian Cuadros <adrian@innku.com>
2010-09-04 13:03:09 -0500

    Comandos con readme

---
+++ db/migrate/20100904175445_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration
+  def self.up
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.integer :units
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :products
+  end
+end
