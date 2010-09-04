94cee5d42d1bc86080faf5348bd9e8d1f70110d0
Adrian Cuadros <adrian@innku.com>
2010-09-04 12:44:15 -0500

    Todo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tienda::Application.config.secret_token = 'bb41f04241a75f0e86a7bb46c0a89fae35b01ecde4d5376ec5ebd6d41595db15db883df83ad75f7b5f255fa17fb89ee54d77b23a97cf482477f3ae614afa6929'
