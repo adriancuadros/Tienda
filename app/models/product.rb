f9168c67bedb1e70c062e61a56873809062d3f1d
Adrian Cuadros <adrian@innku.com>
2010-09-04 13:47:50 -0500

    Validaciones espanol

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
-  validates_presence_of     :name
-  validates_presence_of     :price
+  validates_presence_of     :name, :message => 'El nombre no puede estar en blanco'
+  validates_presence_of     :price, :message => 'El precio no puede estar en blanco'
   
-  validates_numericality_of :price
-  validates_numericality_of :units, :allow_blank => true  
+  validates_numericality_of :price, :message => 'El precio debe ser un numero'
+  validates_numericality_of :units, :allow_blank => true, :message => 'El numero de unidades debe ser un numero'
 end
